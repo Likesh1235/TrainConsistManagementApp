47328a3adba0c8e6cd86fc0ca445b62aaa14b2f0
Likesh Kanna <likeshkanna74@gmail.com>
2026-04-16 18:56:48 +0530

    UC19-Binary Search for Bogie ID

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,27 +1,41 @@
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("==========================================");
-        System.out.println(" UC18 - Linear Search for Bogie ID ");
+        System.out.println(" UC19 - Binary Search for Bogie ID ");
         System.out.println("==========================================\n");
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
+        Arrays.sort(bogieIds);
+
         String searchId = "BG309";
+
+        int low = 0;
+        int high = bogieIds.length - 1;
+        boolean found = false;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int result = bogieIds[mid].compareTo(searchId);
+
+            if (result == 0) {
+                found = true;
+                break;
+            } else if (result < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
 
         System.out.println("Available Bogie IDs:");
         for (String id : bogieIds) {
             System.out.println(id);
-        }
-
-        boolean found = false;
-
-        for (String id : bogieIds) {
-            if (id.equals(searchId)) {
-                found = true;
-                break;
-            }
         }
 
         if (found) {
