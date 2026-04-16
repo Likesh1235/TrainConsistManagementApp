ae83f19c5dab4972e77d6e98d77213faddd4a8c9
Likesh kanna S <137816960+Likesh1235@users.noreply.github.com>
2026-04-16 08:58:48 +0530

    Update TrainConsistManagementApp.java

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,40 +1,27 @@
-import java.util.*;
-
 public class TrainConsistManagementApp {
-
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            if (!swapped) break;
-        }
-    }
 
     public static void main(String[] args) {
 
+        System.out.println("==========================================");
+        System.out.println(" UC16 - Manual Sorting using Bubble Sort ");
+        System.out.println("==========================================\n");
+
         int[] capacities = {72, 56, 24, 70, 60};
 
-        System.out.println("==========================================");
-        System.out.println("UC16 - Manual Sorting using Bubble Sort");
-        System.out.println("==========================================");
-
-        System.out.println("\nOriginal Capacities:");
+        System.out.println("Original Capacities:");
         for (int c : capacities) {
             System.out.print(c + " ");
         }
 
-        bubbleSort(capacities);
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
 
         System.out.println("\n\nSorted Capacities (Ascending):");
         for (int c : capacities) {
