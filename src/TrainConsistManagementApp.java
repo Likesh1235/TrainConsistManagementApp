a4f773c86d11d89ddd467d1415ffbc6ad1e1c8c2
Likesh Kanna <likeshkanna74@gmail.com>
2026-04-16 09:01:18 +0530

    UC17:Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,44 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class TrainConsistManagementApp {
-
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            if (!swapped) break;
-        }
-    }
+public class UseCase17TrainConsistMgmt {
 
     public static void main(String[] args) {
 
-        int[] capacities = {72, 56, 24, 70, 60};
-
         System.out.println("==========================================");
-        System.out.println("UC16 - Manual Sorting using Bubble Sort");
-        System.out.println("==========================================");
+        System.out.println(" UC17 - Sort Bogie Names Using Arrays.sort()");
+        System.out.println("==========================================\n");
 
-        System.out.println("\nOriginal Capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        bubbleSort(capacities);
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
 
-        System.out.println("\n\nSorted Capacities (Ascending):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        Arrays.sort(bogieNames);
+
+        System.out.println("\nSorted Bogie Names (Alphabetical):");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
